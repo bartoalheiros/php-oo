cfe48d78185fa5be3cee4ad9cfc8d929282f4082
BartoAlheiros <alheirosb@gmail.com>
2023-10-10 11:53:06 -0300

    feat: desctruct

--- construtor_destrutor.php
+++ construtor_destrutor.php
@@ -8,10 +8,22 @@
             $this->nome = $nome;
         }
 
+        function __destruct() {
+            echo 'Objeto removido';
+        }
+
         function __get($atributo) {
             return $this->$atributo;
+        }
+
+        function correr() {
+            return $this->__get('nome') . ' está correndo';
         }
     }
 
     $pessoa = new Pessoa('Jorge');
     echo '<br />Nome: ' . $pessoa->__get('nome');
+    echo '<br />' . $pessoa->correr();
+    
+    echo '<br />';
+    // unset($pessoa);
