929142167036e307a20ce039d4e7f8fe952ce813
BartoAlheiros <alheirosb@gmail.com>
2023-10-17 12:07:25 -0300

    att: finalizado encapsulamento com herança

--- oo_pilar_encapsulamento.php
+++ oo_pilar_encapsulamento.php
@@ -1,9 +1,12 @@
 <?php
 
     class Pai {
+		
         private $nome = 'Jorge';
+
         protected $sobrenome = 'Silva';
-        public $humor = 'Feliz';
+        
+		public $humor = 'Feliz';
 
         public function __get($attr) {
             return $this->$attr;
@@ -12,7 +15,7 @@
         public function __set($attr, $value) {
             $this->$attr = $value;
         }
-
+        
         private function executarMania() {
             echo 'Assobiar';
         }
@@ -34,26 +37,33 @@
     }
 
     class Filho extends Pai {
-        // public function getAtributo($attr) {
-        //     return $this->$attr;
-        // }
+       	
+        public function __construct() {
+			//exibir os métodos do objeto
+			echo '<pre>';
+			print_r(get_class_methods($this));
+			echo '</pre>';		
+       	}
 
-        // public function setAtributo($attr, $value) {
-        //     $this->$attr = $value;
-        // }
+	   	private function executarMania() {
+	   		echo 'Cantar';
+	  	}
+
+        protected function responder() {
+            echo 'Olá';
+        }
+
+        public function x() {
+            $this->executarMania();
+        }
 
     }
-
-    // $pai = new Pai();
-    // // echo $pai->humor;
-    // echo $pai->executarAcao();
 
     $filho = new Filho();
     echo '<pre>';
     print_r($filho);
     echo '</pre>';
- 
-    //exibir os métodos do objeto
-    echo '<pre>';
-    print_r(get_class_methods($filho));
-    echo '</pre>';
+
+	$filho->executarAcao();
+    echo '<br />';
+    $filho->x();
