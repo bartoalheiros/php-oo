d7f1ed94eb95ca2b40f1b1138a1c0b0a1f68aacd
BartoAlheiros <alheirosb@gmail.com>
2023-11-12 22:07:04 -0300

    add: mamifero e terrestre interface

--- interfaces.php
+++ interfaces.php
@@ -6,17 +6,58 @@
     }
 
     // -------------------------------------------
-    class Geladeira {
+    class Geladeira implements EquipamentoEletronicoInterface {
         public function abrirPorta() {
             echo 'Abrir a porta';
         }
+
+        public function ligar() {
+            echo 'Ligar';
+        }
+
+        public function desligar() {
+            echo 'Desligar';
+        }
     }
 
-    class TV {
+    class TV implements EquipamentoEletronicoInterface {
         public function trocarCanal() {
             echo 'Trocar canal';
         }
+
+        public function ligar() {
+            echo 'Ligar';
+        }
+
+        public function desligar() {
+            echo 'Desligar';
+        }
+
     }
 
     $x = new Geladeira();
     $y = new TV();
+
+    // -------------------------------------------
+
+    interface MamiferoInterface {
+        public function respirar();
+    }
+
+    interface TerrestreInterface {
+        public function andar();
+    }
+
+    interface AquaticoInterface {
+        public function nadar();
+    }
+
+    class Humano implements MamiferoInterface, TerrestreInterface {  
+        public function andar() {
+            echo 'Andar';
+        }
+        
+        public function respirar() {
+            echo 'Respirar';
+        }
+    }
