2974a99f783825aad984ecb59b5ea86f66fafb88
Barto <alheirosb@gmail.com>
2025-11-05 16:39:41 -0300

    att

--- lib/app_send_mail/processa_envio.php
+++ lib/app_send_mail/processa_envio.php
@@ -54,19 +54,18 @@
         //Server settings
         $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'user@example.com';                     //SMTP username
-        $mail->Password   = 'secret';                               //SMTP password
+        $mail->Username   = 'alheirosbwebweb@gmail.com';                     //SMTP username
+        $mail->Password   = '!@#$B189';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
         //Recipients
-        $mail->setFrom('from@example.com', 'Mailer');
-        $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-        $mail->addAddress('ellen@example.com');               //Name is optional
+        $mail->setFrom('alheirosbwebweb@gmail.com', 'Web Completo Remetente');
+        $mail->addAddress('alheirosbwebweb@gmail.com', 'Web Completo Destinatário');     //Add a recipient
         $mail->addReplyTo('info@example.com', 'Information');
-        $mail->addCC('cc@example.com');
+        //$mail->addCC('cc@example.com');
         $mail->addBCC('bcc@example.com');
 
         //Attachments
@@ -75,7 +74,7 @@
 
         //Content
         $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = 'Here is the subject';
+        $mail->Subject = 'Oi. Eu sou o assunto.';
         $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
         $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
