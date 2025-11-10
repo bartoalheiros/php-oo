0e2c486a17e8acc10358e6c531dd36d468e1112c
Barto <alheirosb@gmail.com>
2025-11-10 09:57:01 -0300

    envio de e-mail funcional

--- lib/app_send_mail/processa_envio.php
+++ lib/app_send_mail/processa_envio.php
@@ -57,20 +57,20 @@
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = 'alheirosbwebweb@gmail.com';                     //SMTP username
-        $mail->Password   = 'elymceljfpwxqeio';                               //SMTP password
+        $mail->Password   = 'lwmw uwdc qaof bcyi';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
         //Recipients
         $mail->setFrom('alheirosbwebweb@gmail.com', 'Web Completo Remetente');
         $mail->addAddress('alheirosbwebweb@gmail.com', 'Web Completo Destinatário');     //Add a recipient
-        $mail->addReplyTo('info@example.com', 'Information');
+        //$mail->addReplyTo('info@example.com', 'Information');
         //$mail->addCC('cc@example.com');
-        $mail->addBCC('bcc@example.com');
+        //$mail->addBCC('bcc@example.com');
 
         //Attachments
-        $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-        $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+        //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+        //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
         //Content
         $mail->isHTML(true);                                  //Set email format to HTML
